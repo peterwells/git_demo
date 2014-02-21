a053b841c412b10472998c634a843417bcaf56e4
Peter Wells <pwells2@nd.edu>
2014-02-21 14:47:16 -0500

    Initial commit

---
+++ demonstation/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demonstation::Application.config.secret_token = '7312cd352f424bb6891430b9bd2e9f64a00e43bbd1a964140cb1a786b3221282c1f023bf2b6843486be82abafb82f05fbc47f4ba88e55bee604d5413d1522a41'
